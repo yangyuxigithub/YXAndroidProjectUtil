70b73100f13a6c444fc2726b9a0e2236c042c1f2
杨玉玺 <yangyuxikaca@163.com>
2019-05-17 17:03:35 +0800

    Initial commit

---
+++ yx_projectutil/src/main/java/com/yangyuxi/yx_projectutil/yx_apputil/YXAppUtil.java
@@ -0,0 +1,223 @@
+package com.yangyuxi.yx_projectutil.yx_apputil;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.DisplayMetrics;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class YXAppUtil {
+
+    /**
+     * 获取包信息.
+     *
+     * @param context the context
+     */
+    public static PackageInfo getPackageInfo(Context context) {
+        PackageInfo info = null;
+        try {
+            String packageName = context.getPackageName();
+            info = context.getPackageManager().getPackageInfo(packageName, 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return info;
+    }
+
+    /**
+     * 获取当前版本号
+     *
+     * @return
+     * @throws Exception
+     */
+    public static String getVersionName(Context context) {
+        StringBuilder builder = new StringBuilder("v");
+        try {
+            PackageInfo packInfo = getPackageInfo(context);
+            builder.append(packInfo.versionName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+
+    /**
+     * 获取开发版本号
+     *
+     * @param context
+     * @return
+     */
+    public static String getVersionCode(Context context) {
+        StringBuilder builder = new StringBuilder();
+        try {
+            PackageInfo packInfo = getPackageInfo(context);
+            builder.append(packInfo.versionCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+
+    /**
+     * 读取application 节点  meta-data 信息
+     */
+    public static String readMetaDataFromApplication(Context context, String key) {
+        try {
+            ApplicationInfo appInfo = context.getPackageManager()
+                    .getApplicationInfo(context.getPackageName(),
+                            PackageManager.GET_META_DATA);
+            return appInfo.metaData.getString(key);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 用来判断服务是否运行.
+     *
+     * @param ctx       the ctx
+     * @param className 判断的服务名字 "com.xxx.xx..XXXService"
+     * @return true 在运行 false 不在运行
+     */
+    public static boolean isServiceRunning(Context ctx, String className) {
+        boolean isRunning = false;
+        ActivityManager activityManager = (ActivityManager) ctx.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> servicesList = activityManager.getRunningServices(Integer.MAX_VALUE);
+        Iterator<ActivityManager.RunningServiceInfo> l = servicesList.iterator();
+        while (l.hasNext()) {
+            ActivityManager.RunningServiceInfo si = (ActivityManager.RunningServiceInfo) l.next();
+            if (className.equals(si.service.getClassName())) {
+                isRunning = true;
+            }
+        }
+        return isRunning;
+    }
+
+    /**
+     * 停止服务.
+     *
+     * @param ctx       the ctx
+     * @param className the class name
+     * @return true, if successful
+     */
+    public static boolean stopRunningService(Context ctx, String className) {
+        Intent intent_service = null;
+        boolean ret = false;
+        try {
+            intent_service = new Intent(ctx, Class.forName(className));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (intent_service != null) {
+            ret = ctx.stopService(intent_service);
+        }
+        return ret;
+    }
+
+    /**
+     * 描述：判断网络是否有效.
+     *
+     * @param context the context
+     * @return true, if is network available
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        try {
+            ConnectivityManager connectivity = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            if (connectivity != null) {
+                NetworkInfo info = connectivity.getActiveNetworkInfo();
+                if (info != null && info.isConnected()) {
+                    if (info.getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return false;
+    }
+
+    /**
+     * Gps是否打开
+     * 需要<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />权限
+     *
+     * @param context the context
+     * @return true, if is gps enabled
+     */
+    public static boolean isGpsEnabled(Context context) {
+        LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        return lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+
+    /**
+     * 判断当前网络是否是移动数据网络.
+     *
+     * @param context the context
+     * @return boolean
+     */
+    public static boolean isMobile(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+        if (activeNetInfo != null
+                && activeNetInfo.getType() == ConnectivityManager.TYPE_MOBILE) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 获取屏幕尺寸与密度.
+     *
+     * @param context the context
+     * @return mDisplayMetrics
+     */
+    public static DisplayMetrics getDisplayMetrics(Context context) {
+        Resources mResources;
+        if (context == null) {
+            mResources = Resources.getSystem();
+
+        } else {
+            mResources = context.getResources();
+        }
+        //DisplayMetrics{density=1.5, width=480, height=854, scaledDensity=1.5, xdpi=160.421, ydpi=159.497}
+        //DisplayMetrics{density=2.0, width=720, height=1280, scaledDensity=2.0, xdpi=160.42105, ydpi=160.15764}
+        DisplayMetrics mDisplayMetrics = mResources.getDisplayMetrics();
+        return mDisplayMetrics;
+    }
+
+    /**
+     * 是否存在该包名的应用 检测设备上是否安装了某应用
+     *
+     * @param context
+     * @return
+     */
+    public static boolean exitAppBy(Context context, String packageName) {
+        final PackageManager packageManager = context.getPackageManager();// 获取packagemanager
+        List<PackageInfo> pinfo = packageManager.getInstalledPackages(0);// 获取所有已安装程序的包信息
+        if (pinfo != null) {
+            for (int i = 0; i < pinfo.size(); i++) {
+                String pn = pinfo.get(i).packageName;
+                if (pn.equals(packageName)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+}
